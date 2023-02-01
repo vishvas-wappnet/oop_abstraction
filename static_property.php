b1c02d3180ff57cbe93e713d0f21817bf672f22b
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-01-31 19:33:19 -0500

    STATIC PROPERTY PROGRAMS DONE

---
+++ static_property.php
@@ -0,0 +1,11 @@
+<?php
+
+                class static_value
+                    {
+                        public static $PI = 3.14;
+                    }
+
+
+                    echo "value of static variable:" . static_value :: $PI;
+
+?>
