ebf0abae70cbdaebc54dc08fb9fa611d2bd99ba7
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-01-31 19:19:50 -0500

    STATIC FUNCTOIN PROGRAMS

---
+++ static_functoin.php
@@ -0,0 +1,13 @@
+ <?php
+            class user
+                {
+                    public static function display()
+                        {
+                            echo"this static function which is called without creating the object";
+                        }
+                }
+ 
+                
+                user::display();
+ 
+ ?>
